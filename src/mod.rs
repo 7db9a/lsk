428d8b4fbb8fb88586c962b18d2552e9626d7c83
7db9a <repo.7db9a@gmail>
2020-05-23 16:29:39 -0400

    refactor: match input is its own method.

--- src/mod.rs
+++ src/mod.rs
@@ -67,13 +67,7 @@ impl LsKey {
             self.run_cmd(list);
     }
 
-    // If you want to return the output of a commands, see fzf example below.
-    // The commmand 'vim', without any args, is a special case handled by
-    // ls-key. If the non-built in command doesn't return output and enters
-    // into a child process (e.g. vim), then shell::cmd cannot be used, to my
-    // understanding.
-    fn run_cmd(mut self, list: List) {
-        let input = terminal::input_n_display::read();
+    fn readline_mode(mut self, list: List, input: Result<(Option<String>), std::io::Error>) {
         match input {
             Ok(t) =>  {
                 if let Some(i) = t {
@@ -259,6 +253,61 @@ impl LsKey {
             },
             Err(_) => ()
         }
+    }
+
+    // If you want to return the output of a commands, see fzf example below.
+    // The commmand 'vim', without any args, is a special case handled by
+    // ls-key. If the non-built in command doesn't return output and enters
+    // into a child process (e.g. vim), then shell::cmd cannot be used, to my
+    // understanding.
+    fn run_cmd(mut self, list: List) {
+        let input = terminal::input_n_display::read();
+        //{
+        //    let stdin = stdin();
+        //    let mut stdout = stdout().into_raw_mode().unwrap();
+
+        //    write!(stdout,
+        //           "{}{}q to exit. Type stuff, use alt, and so on.{}",
+        //           termion::clear::All,
+        //           termion::cursor::Goto(1, 1),
+        //           termion::cursor::Hide)
+        //            .unwrap();
+        //    stdout.flush().unwrap();
+
+        //    for c in stdin.keys() {
+        //        write!(stdout,
+        //               "{}{}",
+        //               termion::cursor::Goto(1, 1),
+        //               termion::clear::CurrentLine)
+        //                .unwrap();
+
+        //        match c.unwrap() {
+        //            Key::Char('q') => break,
+        //            Key::Char(c) => {
+        //                match c {
+        //                    ' ' => {
+        //                        println!("$")
+        //                    },
+        //                    'v' => println!("{}im", c),
+        //                    _ => println!("{}", c),
+        //                }
+        //            }
+        //            Key::Alt(c) => println!("^{}", c),
+        //            Key::Ctrl(c) => println!("*{}", c),
+        //            Key::Esc => println!("ESC"),
+        //            Key::Left => println!("←"),
+        //            Key::Right => println!("→"),
+        //            Key::Up => println!("↑"),
+        //            Key::Down => println!("↓"),
+        //            Key::Backspace => println!("×"),
+        //            _ => {}
+        //        }
+        //        stdout.flush().unwrap();
+        //    }
+
+        //    write!(stdout, "{}", termion::cursor::Show).unwrap();
+        //}
+        self.readline_mode(list, input);
     }
 }
 
