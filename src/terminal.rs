15de2caf1a3cdfb1e205e1a26a1e78de72f485e9
7db9a <repo.7db9a@gmail>
2020-05-23 21:40:10 -0400

    Saving progress.

--- src/terminal.rs
+++ src/terminal.rs
@@ -100,6 +100,52 @@ pub mod input_n_display {
         }
     }
 
+    pub fn read_process_chars() {
+        let stdin = stdin();
+        let mut stdout = stdout().into_raw_mode().unwrap();
+
+        write!(stdout,
+               "{}{}q to exit. Type stuff, use alt, and so on.{}",
+               termion::clear::All,
+               termion::cursor::Goto(1, 1),
+               termion::cursor::Hide)
+                .unwrap();
+        stdout.flush().unwrap();
+
+        for c in stdin.keys() {
+            write!(stdout,
+                   "{}{}",
+                   termion::cursor::Goto(1, 1),
+                   termion::clear::CurrentLine)
+                    .unwrap();
+
+            match c.unwrap() {
+                Key::Char('q') => break,
+                Key::Char(c) => {
+                    match c {
+                        //' ' => {
+                        //    println!("$")
+                        //},
+                        //'v' => println!("{}im", c),
+                        _ => println!("{}", c),
+                    }
+                }
+                Key::Alt(c) => println!("^{}", c),
+                Key::Ctrl(c) => println!("*{}", c),
+                Key::Esc => println!("ESC"),
+                Key::Left => println!("←"),
+                Key::Right => println!("→"),
+                Key::Up => println!("↑"),
+                Key::Down => println!("↓"),
+                Key::Backspace => println!("×"),
+                _ => {}
+            }
+            stdout.flush().unwrap();
+        }
+
+        write!(stdout, "{}", termion::cursor::Show).unwrap();
+    }
+
     pub fn read_char() {
         let stdin = stdin();
         let mut stdout = stdout().into_raw_mode().unwrap();
@@ -311,6 +357,19 @@ mod tests {
     //    let spawn = super::parent_shell::type_text_spawn(text_vec, 50);
     //    spawn.join();
     //}
+
+    #[test]
+    #[ignore]//play
+    fn termion_read_process_chars() {
+	    let test_spawn = thread::spawn(move || {
+            super::input_n_display::read_char()
+	    });
+
+        //let spawn = super::parent_shell::type_text_spawn(vec![r#""$(printf 'q \n ')""#.to_string()], 200);
+
+        test_spawn.join();
+        //spawn.join();
+    }
 
     #[test]
     #[ignore]//play
