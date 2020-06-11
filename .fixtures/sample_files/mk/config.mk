10a65f88155ae5cf3779ab4fd2efdc14b902c525
7db9a <repo.7db9a@gmail>
2020-06-11 16:51:45 -0400

    Saving progress.

---
+++ .fixtures/sample_files/mk/config.mk
@@ -0,0 +1,73 @@
+# Configuration
+## Architecture to build Redox for (aarch64 or x86_64)
+ARCH?=x86_64
+## Flags to pass to the installer (empty to download binary packages)
+INSTALLER_FLAGS?=--cookbook=cookbook
+## Enabled to use binary prefix (much faster)
+PREFIX_BINARY?=1
+## Filesystem size in MB (256 is the default)
+FILESYSTEM_SIZE?=256
+
+# Per host variables
+UNAME := $(shell uname)
+ifeq ($(UNAME),Darwin)
+	FUMOUNT=sudo umount
+	export NPROC=sysctl -n hw.ncpu
+	export REDOX_MAKE=make
+	PREFIX_BINARY=0
+	VB_AUDIO=coreaudio
+	VBM=/Applications/VirtualBox.app/Contents/MacOS/VBoxManage
+else ifeq ($(UNAME),FreeBSD)
+	FUMOUNT=sudo umount
+	export NPROC=sysctl -n hw.ncpu
+	export REDOX_MAKE=gmake
+	PREFIX_BINARY=0
+	VB_AUDIO=pulse # To check, will probaly be OSS on most setups
+	VBM=VBoxManage
+else
+	FUMOUNT=fusermount -u
+	export NPROC=nproc
+	export REDOX_MAKE=make
+	VB_AUDIO=pulse
+	VBM=VBoxManage
+endif
+
+# Automatic variables
+ROOT=$(CURDIR)
+export RUST_COMPILER_RT_ROOT=$(ROOT)/rust/src/llvm-project/compiler-rt
+export RUST_TARGET_PATH=$(ROOT)/kernel/targets
+export XARGO_HOME=$(ROOT)/build/xargo
+export XARGO_RUST_SRC=$(ROOT)/rust/src
+
+## Kernel variables
+KTARGET=$(ARCH)-unknown-none
+KBUILD=build/kernel
+
+## Userspace variables
+export TARGET=$(ARCH)-unknown-redox
+BUILD=build/userspace
+INSTALLER=\
+	export REDOXER_TOOLCHAIN="$(ROOT)/$(PREFIX_INSTALL)" && \
+	export PATH="$(PREFIX_PATH):$$PATH" && \
+ 	installer/target/release/redox_installer $(INSTALLER_FLAGS)
+
+## Bootloader variables
+EFI_TARGET=$(ARCH)-efi-pe
+
+## Cross compiler variables
+AR=$(TARGET)-gcc-ar
+AS=$(TARGET)-as
+CC=$(TARGET)-gcc
+CXX=$(TARGET)-g++
+LD=$(TARGET)-ld
+NM=$(TARGET)-gcc-nm
+OBJCOPY=$(TARGET)-objcopy
+OBJDUMP=$(TARGET)-objdump
+RANLIB=$(TARGET)-gcc-ranlib
+READELF=$(TARGET)-readelf
+STRIP=$(TARGET)-strip
+
+## Rust cross compile variables
+export AR_$(subst -,_,$(TARGET))=$(TARGET)-ar
+export CC_$(subst -,_,$(TARGET))=$(TARGET)-gcc
+export CXX_$(subst -,_,$(TARGET))=$(TARGET)-g++
---
+++ .fixtures/sample_files/rust/redox/mk/prefix.mk
@@ -0,0 +1,226 @@
+PREFIX=prefix/$(TARGET)
+
+PREFIX_INSTALL=$(PREFIX)/relibc-install
+PREFIX_PATH=$(ROOT)/$(PREFIX_INSTALL)/bin
+
+export PREFIX_RUSTFLAGS=-L $(ROOT)/$(PREFIX_INSTALL)/$(TARGET)/lib
+export RUSTUP_TOOLCHAIN=$(ROOT)/$(PREFIX_INSTALL)
+
+prefix: $(PREFIX_INSTALL)
+
+PREFIX_STRIP=\
+	mkdir -p bin libexec "$(TARGET)/bin" && \
+	find bin libexec "$(TARGET)/bin" "$(TARGET)/lib" \
+		-type f \
+		-exec strip --strip-unneeded {} ';' \
+		2> /dev/null
+
+$(PREFIX)/relibc-install: $(ROOT)/relibc | $(PREFIX)/rust-install
+	rm -rf "$@.partial" "$@"
+	cp -r "$(PREFIX)/rust-install" "$@.partial"
+	rm -rf "$@.partial/$(TARGET)/include/"*
+	cp -r "$(PREFIX)/rust-install/$(TARGET)/include/c++" "$@.partial/$(TARGET)/include/c++"
+	cd "$<" && \
+	export PATH="$(ROOT)/$@.partial/bin:$$PATH" && \
+	export CARGO="env -u CARGO xargo" && \
+	$(MAKE) -j `$(NPROC)` all && \
+	$(MAKE) -j `$(NPROC)` install DESTDIR="$(ROOT)/$@.partial/$(TARGET)"
+	cd "$@.partial" && $(PREFIX_STRIP)
+	touch "$@.partial"
+	mv "$@.partial" "$@"
+
+$(PREFIX)/relibc-install.tar.gz: $(PREFIX)/relibc-install
+	tar \
+		--create \
+		--gzip \
+		--file "$@" \
+		--directory="$<" \
+		.
+
+ifeq ($(PREFIX_BINARY),1)
+
+$(PREFIX)/rust-install.tar.gz:
+	mkdir -p "$(@D)"
+	wget -O $@.partial "https://static.redox-os.org/toolchain/$(TARGET)/rust-install.tar.gz"
+	mv $@.partial $@
+
+$(PREFIX)/rust-install: $(PREFIX)/rust-install.tar.gz
+	rm -rf "$@.partial" "$@"
+	mkdir -p "$@.partial"
+	tar --extract --file "$<" --directory "$@.partial" --strip-components=1
+	touch "$@.partial"
+	mv "$@.partial" "$@"
+
+else
+
+PREFIX_BASE_INSTALL=$(PREFIX)/rust-freestanding-install
+PREFIX_FREESTANDING_INSTALL=$(PREFIX)/gcc-freestanding-install
+
+PREFIX_BASE_PATH=$(ROOT)/$(PREFIX_BASE_INSTALL)/bin
+PREFIX_FREESTANDING_PATH=$(ROOT)/$(PREFIX_FREESTANDING_INSTALL)/bin
+
+$(PREFIX)/binutils.tar.bz2:
+	mkdir -p "$(@D)"
+	wget -O $@.partial "https://gitlab.redox-os.org/redox-os/binutils-gdb/-/archive/redox/binutils-gdb-redox.tar.bz2"
+	mv $@.partial $@
+
+$(PREFIX)/binutils: $(PREFIX)/binutils.tar.bz2
+	rm -rf "$@.partial" "$@"
+	mkdir -p "$@.partial"
+	tar --extract --file "$<" --directory "$@.partial" --strip-components=1
+	touch "$@.partial"
+	mv "$@.partial" "$@"
+
+$(PREFIX)/binutils-install: $(PREFIX)/binutils
+	rm -rf "$<-build" "$@.partial" "$@"
+	mkdir -p "$<-build" "$@.partial"
+	cd "$<-build" && \
+	"$(ROOT)/$</configure" \
+		--target="$(TARGET)" \
+		--program-prefix="$(TARGET)-" \
+		--prefix="" \
+		--disable-werror \
+		&& \
+	$(MAKE) -j `$(NPROC)` all && \
+	$(MAKE) -j `$(NPROC)` install DESTDIR="$(ROOT)/$@.partial"
+	rm -rf "$<-build"
+	cd "$@.partial" && $(PREFIX_STRIP)
+	touch "$@.partial"
+	mv "$@.partial" "$@"
+
+$(PREFIX)/gcc.tar.bz2:
+	mkdir -p "$(@D)"
+	wget -O $@.partial "https://gitlab.redox-os.org/redox-os/gcc/-/archive/redox/gcc-redox.tar.bz2"
+	mv "$@.partial" "$@"
+
+$(PREFIX)/gcc: $(PREFIX)/gcc.tar.bz2
+	mkdir -p "$@.partial"
+	tar --extract --file "$<" --directory "$@.partial" --strip-components=1
+	cd "$@.partial" && ./contrib/download_prerequisites
+	touch "$@.partial"
+	mv "$@.partial" "$@"
+
+$(PREFIX)/gcc-freestanding-install: $(PREFIX)/gcc | $(PREFIX)/binutils-install
+	rm -rf "$<-freestanding-build" "$@.partial" "$@"
+	mkdir -p "$<-freestanding-build"
+	cp -r "$(PREFIX)/binutils-install" "$@.partial"
+	cd "$<-freestanding-build" && \
+	export PATH="$(ROOT)/$@.partial/bin:$$PATH" && \
+	"$(ROOT)/$</configure" \
+		--target="$(TARGET)" \
+		--program-prefix="$(TARGET)-" \
+		--prefix="" \
+		--disable-nls \
+		--enable-languages=c,c++ \
+		--without-headers \
+		&& \
+	$(MAKE) -j `$(NPROC)` all-gcc && \
+	$(MAKE) -j `$(NPROC)` install-gcc DESTDIR="$(ROOT)/$@.partial"
+	rm -rf "$<-freestanding-build"
+	cd "$@.partial" && $(PREFIX_STRIP)
+	touch "$@.partial"
+	mv "$@.partial" "$@"
+
+$(PREFIX)/rust-freestanding-install: $(ROOT)/rust | $(PREFIX)/binutils-install
+	rm -rf "$(PREFIX)/rust-freestanding-build" "$@.partial" "$@"
+	mkdir -p "$(PREFIX)/rust-freestanding-build"
+	cp -r "$(PREFIX)/binutils-install" "$@.partial"
+	cd "$(PREFIX)/rust-freestanding-build" && \
+	export PATH="$(ROOT)/$@.partial/bin:$$PATH" && \
+	"$</configure" \
+		--prefix="/" \
+		--disable-docs \
+		--enable-cargo-native-static \
+		--enable-extended \
+		--enable-llvm-static-stdcpp \
+		--tools=cargo \
+		&& \
+	$(MAKE) -j `$(NPROC)` && \
+	$(MAKE) -j `$(NPROC)` install DESTDIR="$(ROOT)/$@.partial"
+	rm -rf "$(PREFIX)/rust-freestanding-build"
+	mkdir -p "$@.partial/lib/rustlib/x86_64-unknown-linux-gnu/bin"
+	cd "$@.partial" && $(PREFIX_STRIP)
+	touch "$@.partial"
+	mv "$@.partial" "$@"
+
+$(PREFIX)/relibc-freestanding-install: $(ROOT)/relibc | $(PREFIX_BASE_INSTALL) $(PREFIX_FREESTANDING_INSTALL)
+	rm -rf "$@.partial" "$@"
+	mkdir -p "$@.partial"
+	cd "$<" && \
+	export PATH="$(PREFIX_BASE_PATH):$(PREFIX_FREESTANDING_PATH):$$PATH" && \
+	export CARGO="env -u CARGO -u RUSTUP_TOOLCHAIN xargo" && \
+	export CC_$(subst -,_,$(TARGET))="$(TARGET)-gcc -isystem $(ROOT)/$@.partial/$(TARGET)/include" && \
+	$(MAKE) -j `$(NPROC)` all && \
+	$(MAKE) -j `$(NPROC)` install DESTDIR="$(ROOT)/$@.partial/$(TARGET)"
+	cd "$@.partial" && $(PREFIX_STRIP)
+	touch "$@.partial"
+	mv "$@.partial" "$@"
+
+$(PREFIX)/gcc-install: $(PREFIX)/gcc | $(PREFIX)/relibc-freestanding-install
+	rm -rf "$<-build" "$@.partial" "$@"
+	mkdir -p "$<-build"
+	cp -r "$(PREFIX_BASE_INSTALL)" "$@.partial"
+	cd "$<-build" && \
+	export PATH="$(ROOT)/$@.partial/bin:$$PATH" && \
+	"$(ROOT)/$</configure" \
+		--target="$(TARGET)" \
+		--program-prefix="$(TARGET)-" \
+		--prefix="" \
+		--with-sysroot \
+		--with-build-sysroot="$(ROOT)/$(PREFIX)/relibc-freestanding-install/$(TARGET)" \
+		--with-native-system-header-dir="/include" \
+		--disable-multilib \
+		--disable-nls \
+		--disable-werror \
+		--enable-languages=c,c++ \
+		--enable-shared \
+		--enable-threads=posix \
+		&& \
+	$(MAKE) -j `$(NPROC)` all-gcc all-target-libgcc all-target-libstdc++-v3 && \
+	$(MAKE) -j `$(NPROC)` install-gcc install-target-libgcc install-target-libstdc++-v3 DESTDIR="$(ROOT)/$@.partial" && \
+	rm $(ROOT)/$@.partial/$(TARGET)/lib/*.la
+	rm -rf "$<-build"
+	cd "$@.partial" && $(PREFIX_STRIP)
+	touch "$@.partial"
+	mv "$@.partial" "$@"
+
+$(PREFIX)/gcc-install.tar.gz: $(PREFIX)/gcc-install
+	tar \
+		--create \
+		--gzip \
+		--file "$@" \
+		--directory="$<" \
+		.
+
+$(PREFIX)/rust-install: $(ROOT)/rust | $(PREFIX)/gcc-install
+	rm -rf "$(PREFIX)/rust-build" "$@.partial" "$@"
+	mkdir -p "$(PREFIX)/rust-build"
+	cp -r "$(PREFIX)/gcc-install" "$@.partial"
+	cd "$(PREFIX)/rust-build" && \
+	export PATH="$(ROOT)/$@.partial/bin:$$PATH" && \
+	"$</configure" \
+		--prefix="/" \
+		--disable-docs \
+		--enable-cargo-native-static \
+		--enable-extended \
+		--enable-llvm-static-stdcpp \
+		--tools=cargo \
+		--target="$(TARGET)" \
+		&& \
+	$(MAKE) -j `$(NPROC)` && \
+	rm -rf "$(ROOT)/$@.partial/lib/rustlib" "$(ROOT)/$@.partial/share/doc/rust" && \
+	$(MAKE) -j `$(NPROC)` install DESTDIR="$(ROOT)/$@.partial"
+	rm -rf "$(PREFIX)/rust-build"
+	mkdir -p "$@.partial/lib/rustlib/x86_64-unknown-linux-gnu/bin"
+	cd "$@.partial" && find . -name *.old -exec rm {} ';' && $(PREFIX_STRIP)
+	touch "$@.partial"
+	mv "$@.partial" "$@"
+
+$(PREFIX)/rust-install.tar.gz: $(PREFIX)/rust-install
+	tar \
+		--create \
+		--gzip \
+		--file "$@" \
+		--directory="$<" \
+		.
+endif
