7623a6406e7dd815092613ee704459d6ad781d9b
7db9a <repo.7db9a@gmail>
2020-06-22 22:11:13 -0400

    Saving progress.

--- fzc/fzc.sh
+++ fzc/fzc.sh
@@ -2,9 +2,9 @@
 
 # $1 is the number of lines to display.
 fuzzy_zsh_history() {
-    cat $HOME/.zsh_history | cut -c 16- | fzy -l $1
+    cat $HOME/.zsh_history | cut -c 16- | fzf
 }
 
 if [ "$1" = "zsh" ]; then
-    fuzzy_zsh_history $2
+    fuzzy_zsh_history
 fi
