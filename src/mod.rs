4c3426387346026023777dd86e46c23b5c39d045
7db9a <repo.7db9a@gmail>
2020-06-27 13:05:19 -0400

    Saving progress.

--- src/mod.rs
+++ src/mod.rs
@@ -219,28 +219,28 @@ impl LsKey {
                     let width = r.1;
                     let height = r.2;
                     let display = grid.fit_into_width(width);
-                    //if display.is_some() && !self.test {
-                    //     let display = display.unwrap(); // Safe to unwrap.
-                    //     let row_count = display.row_count();
-                    //     if row_count > height {
-                    //         //panic!("Can't fit list into screen.");
-                    //         //
-                    //         let range = 1..(entries_count - incr);
+                    if display.is_some() && !self.test {
+                         let display = display.unwrap(); // Safe to unwrap.
+                         let row_count = display.row_count();
+                         if row_count > height {
+                             //panic!("Can't fit list into screen.");
+                             //
+                             let range = 1..(entries_count - incr);
 
-                    //         let mut filter_vec: Vec<usize> = vec![];
+                             let mut filter_vec: Vec<usize> = vec![];
 
-                    //         range.into_iter().for_each(|i|
-                    //             filter_vec.push(i)
-                    //         );
+                             range.into_iter().for_each(|i|
+                                 filter_vec.push(i)
+                             );
 
-                    //         list_filter = Some(filter_vec);
+                             list_filter = Some(filter_vec);
 
-                    //         incr += 1;
-                    //     } else {
-                    //        go = false; 
-                    //     }
-                    //     self.display = Some((self.list.parent_path.clone(), display.to_string()));
-                    //} else {
+                             incr += 1;
+                         } else {
+                            go = false; 
+                         }
+                         self.display = Some((self.list.parent_path.clone(), display.to_string()));
+                    } else {
                         let display = grid.fit_into_columns(1);
                          let row_count = display.row_count();
                          if row_count > height {
@@ -261,7 +261,7 @@ impl LsKey {
                             go = false; 
                          }
                          self.display = Some((self.list.parent_path.clone(), display.to_string()));
-                    //}
+                    }
                 } else {
                     go = false;
                 }
